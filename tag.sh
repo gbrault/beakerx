378403997cb516a0aee92bade263c4f323dfe228
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-06-28 16:35:06 +0200

    update docker container with influxdb and new pandas, etc.

--- tag.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-### build the image locally (faster than going through dockerhub. Dockerhub often times out...)
-docker build --target builder --tag lobnek/docker .
-
-### tags an existing image and pushes it to dockerhub where it shows up with the tag
-docker tag lobnek/docker lobnek/docker:v0.8
-docker push lobnek/docker
