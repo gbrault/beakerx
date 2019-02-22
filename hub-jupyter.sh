2a033fdd6cf8ed7895d335337646cddbe2d67692
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-02-22 15:47:38 +0100

    lobnek Jupyter

---
+++ hub-jupyter.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+image='lobnek/jupyter'
+
+# https://stackoverflow.com/questions/27643017/do-i-need-to-manually-tag-latest-when-pushing-to-docker-public-repository
+docker build -f ./Dockerfile-Jupyter --tag ${image}:latest --no-cache .
+docker push ${image}:latest
+
+VERSION='v1.0'
+echo ${VERSION}
+
+docker tag ${image} ${image}:${VERSION}
+docker push ${image}:${VERSION}
+docker rmi -f ${image}:${VERSION}
+
