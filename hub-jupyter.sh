2d1620d0c0ba7a06577d50d0dd7e6a48c63122ff
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-03-11 17:13:32 +0100

    adding seaborn and Matplotlib in Jupyter package (beakerx just too unreliable)

--- hub-jupyter.sh
+++ hub-jupyter.sh
@@ -2,10 +2,10 @@
 image='lobnek/jupyter'
 
 # https://stackoverflow.com/questions/27643017/do-i-need-to-manually-tag-latest-when-pushing-to-docker-public-repository
-docker build -f ./Dockerfile-Jupyter --tag ${image}:latest --no-cache .
+docker build -f ./Dockerfile-Jupyter --tag ${image}:latest .
 docker push ${image}:latest
 
-VERSION='v1.3'
+VERSION='v1.6'
 echo ${VERSION}
 
 docker tag ${image} ${image}:${VERSION}
--- hub.sh
+++ hub.sh
@@ -6,7 +6,7 @@ image='lobnek/docker'
 if [ $? -eq 0 ]
 then
     # https://stackoverflow.com/questions/27643017/do-i-need-to-manually-tag-latest-when-pushing-to-docker-public-repository
-    docker build --tag ${image}:latest --no-cache --target builder .
+    docker build --tag ${image}:latest --target builder .
     docker push ${image}:latest
 
     VERSION='v1.6'
