600bcc2787795837d61092c01615ef18518acc37
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-03-12 16:53:49 +0100

    Dockerfile for Jupyter

--- hub-jupyter.sh
+++ hub-jupyter.sh
@@ -5,7 +5,7 @@ image='lobnek/jupyter'
 docker build -f ./Dockerfile-Jupyter --tag ${image}:latest .
 docker push ${image}:latest
 
-VERSION='v2.0'
+VERSION='v2.1'
 echo ${VERSION}
 
 docker tag ${image} ${image}:${VERSION}
